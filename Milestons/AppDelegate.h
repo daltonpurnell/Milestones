73707e45e8f8cdcabbb15ab8a3dab7fc328227af
daltonpurnell <daltonpurnell@live.com>
2015-05-02 19:40:27 -0600

    Initial Commit

---
+++ Milestons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Milestons
+//
+//  Created by Dalton on 5/2/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Milestons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Milestons
+//
+//  Created by Dalton on 5/2/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
