3b59c917ecb6cb54de81849469a335a1bfc37920
daltonpurnell <daltonpurnell@live.com>
2015-05-14 20:20:40 -0600

    adds code for login view controller

--- Milestons/LoginViewController.m
+++ Milestons/LoginViewController.m
@@ -13,6 +13,8 @@
 
 @interface LoginViewController () <PFSignUpViewControllerDelegate, PFLogInViewControllerDelegate>
 
+@property (nonatomic, strong) PFUser *currentUser;
+
 @end
 
 @implementation LoginViewController
@@ -30,12 +32,67 @@
 
 - (IBAction)loginButtonTapped:(id)sender {
     
+    PFLogInViewController *logIn = [PFLogInViewController new];
+    logIn.delegate = self;
+    [self presentViewController:logIn animated:YES completion:nil];
     
 }
 
 
 - (IBAction)signUpButtonTapped:(id)sender {
     
+    PFSignUpViewController *signUp = [PFSignUpViewController new];
+    signUp.delegate = self;
+    [self presentViewController:signUp animated:YES completion:nil];
+    
+}
+
+// Delegate methods for authentication view controllers
+
+- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
+    self.currentUser = user;
+    
+    [self addUserData];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
+    self.currentUser = user;
+    
+    [self addUserData];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (void)addUserData {
+    
+    
+    // Is @"yourData" just a placeholder?  I copied this from a parse practice project we did in class
+    PFQuery *query = [PFQuery queryWithClassName:@"yourData"];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        
+        if ([objects count] == 0) {
+            
+            PFObject *yourData = [PFObject objectWithClassName:@"yourData"];
+            yourData[@"dictionaryKey"] = @"dictionaryValue";
+            
+            // If there is a current user you can set that user as the only user that can access this object:
+            if (self.currentUser) {
+                yourData.ACL = [PFACL ACLWithUser:self.currentUser];
+            }
+            
+            [yourData saveInBackground];
+            
+        } else {
+            
+            NSLog(@"You already stored your data");
+        }
+        
+    }];
     
 }
 
