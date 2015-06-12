fdb76c8253e50312cb5eb7149ff62a9a4c660664
daltonpurnell <daltonpurnell@live.com>
2015-06-11 18:48:22 -0600

    still in progress

--- MilestonesKit/MilestonesKit.h
+++ MilestonesKit/MilestonesKit.h
@@ -29,25 +29,18 @@
 #import <AudioToolbox/AudioToolbox.h>
 #import <CFNetwork/CFNetwork.h>
 #import <ParseCrashReporting/ParseCrashReporting.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <QuartzCore/QuartzCore.h>
+#import <Security/Security.h>
+#import <StoreKit/StoreKit.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
 
 //#import <ParseFacebookUtils/ParseFacebookUtils.h>
-
-//@import Parse;
-//@import ParseUI;
-//@import ParseFacebookUtils;
-//@import ParseFacebookUtilsV4;
-//@import ParseCrashReporting;
-//@import AudioToolbox;
-//@import CFNetwork;
-//@import CoreGraphics;
-//@import CoreLocation;
-//@import MobileCoreServices;
-//@import QuartzCore;
-//@import Security;
-//@import Storekit;
-//@import SystemConfiguration;
-//@import Accounts;
-//@import Social;
 //@import libsqlite3;
 //@import libz;
 
